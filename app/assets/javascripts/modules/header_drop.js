edeb2c7bd5b7d69bd82029c97801348532282375
f4rk19 <67687475+f4rk19@users.noreply.github.com>
2020-08-28 16:39:54 +0900

    header
    don't use master.

--- app/assets/javascripts/modules/header_drop.js
+++ app/assets/javascripts/modules/header_drop.js
@@ -1,5 +1,18 @@
 $(document).on('turbolinks:load', function(){
   $(".header__nav").on("click", function(){
-    console.log("yes");
+    $(".header__drop-menu").removeClass("not-show");
+    $(".header__drop-menu").addClass("is-show");
+  });
+  $(".drop-menu__btn").on("click", function(){
+    $(".header__drop-menu").removeClass("is-show");
+    $(".header__drop-menu").addClass("not-show");
+  });
+  $(window).resize(function(){
+    var w = $(window).width();
+    var x = 960;
+    if (w > x) {
+      $(".header__drop-menu").removeClass("is-show");
+      $(".header__drop-menu").addClass("not-show");
+    }
   });
 });

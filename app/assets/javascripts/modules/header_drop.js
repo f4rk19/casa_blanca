019891dee43354118f157e4935c76c883a6cbbf1
f4rk19 <67687475+f4rk19@users.noreply.github.com>
2020-08-30 02:21:31 +0900

    not yet

--- app/assets/javascripts/modules/header_drop.js
+++ app/assets/javascripts/modules/header_drop.js
@@ -2,10 +2,12 @@ $(document).on('turbolinks:load', function(){
   $(".header__nav").on("click", function(){
     $(".header__drop-menu").removeClass("not-show");
     $(".header__drop-menu").addClass("is-show");
+    $(".main").css({'opacity': '0.5'});
   });
   $(".drop-menu__btn").on("click", function(){
     $(".header__drop-menu").removeClass("is-show");
     $(".header__drop-menu").addClass("not-show");
+    $(".main").css({'opacity': '1'});
   });
   $(window).resize(function(){
     var w = $(window).width();
@@ -13,6 +15,7 @@ $(document).on('turbolinks:load', function(){
     if (w > x) {
       $(".header__drop-menu").removeClass("is-show");
       $(".header__drop-menu").addClass("not-show");
+      $(".main").css({'opacity': '1'});
     }
   });
 });

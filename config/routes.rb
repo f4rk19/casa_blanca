72c069b24a5da8e3254cb04b9bf9bb8643d75575
f4rk19 <67687475+f4rk19@users.noreply.github.com>
2020-09-01 07:21:55 +0900

    devise
    top page mark up & devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   get 'casa/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "casa#index"
